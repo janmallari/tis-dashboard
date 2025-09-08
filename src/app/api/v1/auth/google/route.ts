d58fea0a66fd92c986710adba3be0ca028104822
Jan Mallari <mallari.janlester@gmail.com>
2025-09-08 10:28:26 +0800

    feat: working

--- src/app/api/v1/auth/google/route.ts
+++ src/app/api/v1/auth/google/route.ts
@@ -14,6 +14,7 @@ export async function GET(request: Request) {
     scope: [
       'https://www.googleapis.com/auth/drive.file',
       'https://www.googleapis.com/auth/presentations',
+      'https://www.googleapis.com/auth/spreadsheets',
     ],
   });
 
